9e9af32d9150fbdcd221ebea20a184eef1ae0647
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-15 18:54:01 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,16 @@
-# Write your code here. If you get stuck, at the very bottom of this lab we've included
-# the answers. You should ONLY check those answers if you get totally stuck. The important
-# thing is NOT to "complete labs," the important thing is to learn the skills you need
-# in order to complete the rest of this course and be ready for the next course!
+name="kimia"
+def introduction parameters
+  puts "Hi, my name is #{parameters}."
+end
+introduction name
+
+language="ruby" name
+def introduction_with_language param
+  puts "Hi,my name is #
+  
+end
+
+
 
 
 
