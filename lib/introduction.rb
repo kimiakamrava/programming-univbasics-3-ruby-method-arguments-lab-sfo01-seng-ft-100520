d25ac159dc8942561555d46eedec6135ea5131e6
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-15 19:31:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,12 +13,6 @@ introduction
 def introduction_with_language (name="kimia",language="ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language_optional
-
-def introduction_with_language (name,language="ruby")
-   "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-introduction_with_language
 
 
 
